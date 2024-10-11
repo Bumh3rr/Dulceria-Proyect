5cb0ddccf936205fd308d1a2299760de2956fe0d
CrisGoat <cristian2005barragan@gmail.com>
2024-10-11 00:20:44 -0400

    Skibidi

--- DulceriaApp/src/form/request/RequestProducto.java
+++ DulceriaApp/src/form/request/RequestProducto.java
@@ -6,8 +6,18 @@ import model.Producto;
 
 import java.util.LinkedList;
 
+/**
+ * Clase RequestProducto que maneja las solicitudes relacionadas con los productos.
+ */
 public class RequestProducto {
 
+    /**
+     * Agrega un producto a la base de datos.
+     *
+     * @param producto El objeto Producto que se desea agregar.
+     * @return Booleano que indica si el producto fue agregado exitosamente.
+     * @throws Exception Si ocurre un error durante la operación.
+     */
     public static Boolean addProducto(Producto producto) throws Exception {
         return PoolThreads.getInstance().getExecutorService().submit(() -> {
             try {
@@ -18,6 +28,12 @@ public class RequestProducto {
         }).get();
     }
 
+    /**
+     * Obtiene una lista de todos los productos de la base de datos.
+     *
+     * @return LinkedList de objetos Producto.
+     * @throws Exception Si ocurre un error durante la operación.
+     */
     public static LinkedList<Producto> getAllProductos() throws Exception {
         return PoolThreads.getInstance().getExecutorService().submit(() -> {
             try {
