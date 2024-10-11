b1d302a9371235c2fa690ab6de634af3a21fcbbf
CrisGoat <cristian2005barragan@gmail.com>
2024-10-11 00:23:25 -0400

    Doc

--- DulceriaApp/src/form/request/RequestCategoria.java
+++ DulceriaApp/src/form/request/RequestCategoria.java
@@ -6,7 +6,18 @@ import model.Categoria;
 
 import java.util.LinkedList;
 
+/**
+ * Clase RequestCategoria que maneja las solicitudes relacionadas con las categorías.
+ */
 public class RequestCategoria {
+
+    /**
+     * Agrega una categoría a la base de datos.
+     *
+     * @param categoriaN El objeto Categoria que se desea agregar.
+     * @return Booleano que indica si la categoría fue agregada exitosamente.
+     * @throws Exception Si ocurre un error durante la operación.
+     */
     public static Boolean addCategoria(Categoria categoriaN) throws Exception {
         return PoolThreads.getInstance().getExecutorService().submit(() -> {
             try {
@@ -17,6 +28,12 @@ public class RequestCategoria {
         }).get();
     }
 
+    /**
+     * Obtiene una lista de todas las categorías de la base de datos.
+     *
+     * @return LinkedList de objetos Categoria.
+     * @throws Exception Si ocurre un error durante la operación.
+     */
     public static LinkedList<Categoria> getCategoriasAll() throws Exception {
         return PoolThreads.getInstance().getExecutorService().submit(() -> {
             try {
